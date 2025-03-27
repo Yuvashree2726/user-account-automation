96a2e62ca4e021cef8d72aa836b6ccda864bb31d
Yuvashree <gyuvashree27@gmail.com>
2025-03-27 10:41:27 +0530

    Initial Commit: Add user account script

---
+++ user_account.py
@@ -0,0 +1,26 @@
+import csv
+import secrets
+import subprocess
+from pathlib import Path
+
+cwd = Path.cwd()
+
+with open(cwd / "users_in.csv", "r") as file_input, open(cwd / "users_out.csv", "w") as file_output:
+    reader = csv.DictReader(file_input)
+    writer = csv.DictWriter(file_output, fieldnames=reader.fieldnames)
+    writer.writeheader()
+
+    for user in reader:
+        user["password"] = secrets.token_hex(8)
+        useradd_cmd = ["/sbin/useradd",
+                       "-c", user["real_name"],
+                       "-m",
+                       "-G", "users",
+                       "-p", user["password"],
+                       user["username"]]
+        try:
+            subprocess.run(useradd_cmd, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error creating user {user['username']}: {e}")
+        
+        writer.writerow(user)
